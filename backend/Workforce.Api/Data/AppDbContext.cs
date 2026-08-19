d0f4831f46d8262d4c2a3bf5fb48e2f9b0784a8c
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-20 00:39:14 +0200

    fix: configure employee coverage navigation

--- backend/Workforce.Api/Data/AppDbContext.cs
+++ backend/Workforce.Api/Data/AppDbContext.cs
@@ -20,53 +20,26 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbCon
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
         modelBuilder.Entity<Employee>().Property(x => x.PositionPercent).HasPrecision(5, 2);
         modelBuilder.Entity<Shift>().Property(x => x.Hours).HasPrecision(4, 2);
-
         modelBuilder.Entity<EmployeeCompetence>().HasKey(x => new { x.EmployeeId, x.CompetenceId });
         modelBuilder.Entity<ShiftAssignment>().HasKey(x => new { x.ShiftId, x.EmployeeId });
         modelBuilder.Entity<ShiftRequirement>().HasKey(x => new { x.ShiftId, x.CompetenceId });
         modelBuilder.Entity<EmployeeAvailability>().HasKey(x => new { x.EmployeeId, x.Date });
-
         modelBuilder.Entity<Competence>().HasIndex(x => x.Name).IsUnique();
 
         modelBuilder.Entity<WorkTask>()
-            .HasOne(x => x.Competence)
-            .WithMany()
-            .HasForeignKey(x => x.CompetenceId)
-            .OnDelete(DeleteBehavior.SetNull);
-
+            .HasOne(x => x.Competence).WithMany().HasForeignKey(x => x.CompetenceId).OnDelete(DeleteBehavior.SetNull);
         modelBuilder.Entity<ShiftTask>()
-            .HasOne(x => x.Shift)
-            .WithMany(x => x.ShiftTasks)
-            .HasForeignKey(x => x.ShiftId)
-            .OnDelete(DeleteBehavior.Cascade);
-
+            .HasOne(x => x.Shift).WithMany(x => x.ShiftTasks).HasForeignKey(x => x.ShiftId).OnDelete(DeleteBehavior.Cascade);
         modelBuilder.Entity<ShiftTask>()
-            .HasOne(x => x.WorkTask)
-            .WithMany(x => x.ShiftTasks)
-            .HasForeignKey(x => x.WorkTaskId)
-            .OnDelete(DeleteBehavior.Restrict);
-
+            .HasOne(x => x.WorkTask).WithMany(x => x.ShiftTasks).HasForeignKey(x => x.WorkTaskId).OnDelete(DeleteBehavior.Restrict);
         modelBuilder.Entity<ShiftTaskCoverage>()
-            .HasOne(x => x.ShiftTask)
-            .WithMany(x => x.ShiftTaskCoverages)
-            .HasForeignKey(x => x.ShiftTaskId)
-            .OnDelete(DeleteBehavior.Cascade);
-
+            .HasOne(x => x.ShiftTask).WithMany(x => x.ShiftTaskCoverages).HasForeignKey(x => x.ShiftTaskId).OnDelete(DeleteBehavior.Cascade);
         modelBuilder.Entity<ShiftTaskCoverage>()
-            .HasOne(x => x.Employee)
-            .WithMany()
-            .HasForeignKey(x => x.EmployeeId)
-            .OnDelete(DeleteBehavior.Restrict);
-
+            .HasOne(x => x.Employee).WithMany(x => x.ShiftTaskCoverages).HasForeignKey(x => x.EmployeeId).OnDelete(DeleteBehavior.Restrict);
         modelBuilder.Entity<CoverageAuditEntry>()
-            .HasOne(x => x.Shift)
-            .WithMany(x => x.CoverageAudits)
-            .HasForeignKey(x => x.ShiftId)
-            .OnDelete(DeleteBehavior.Cascade);
-
+            .HasOne(x => x.Shift).WithMany(x => x.CoverageAudits).HasForeignKey(x => x.ShiftId).OnDelete(DeleteBehavior.Cascade);
         modelBuilder.Entity<CoverageAuditEntry>().HasIndex(x => new { x.ShiftId, x.EvaluatedAt });
         modelBuilder.Entity<CoverageAuditEntry>().Property(x => x.Status).HasMaxLength(20).IsRequired();
         modelBuilder.Entity<CoverageAuditEntry>().Property(x => x.TriggeredBy).HasMaxLength(200);
