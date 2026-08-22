9c89da60716f112888035d52a7b4c52b1e6b070e
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-22 17:32:17 +0200

    fix: make EF model mapping deterministic

--- backend/Workforce.Api/Data/AppDbContext.cs
+++ backend/Workforce.Api/Data/AppDbContext.cs
@@ -17,17 +17,117 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbCon
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Employee>().Property(x => x.PositionPercent).HasPrecision(5, 2);
-        modelBuilder.Entity<Employee>().Property(x => x.MaxWeeklyHours).HasPrecision(5, 2);
-        modelBuilder.Entity<Shift>().Property(x => x.Hours).HasPrecision(4, 2);
-        modelBuilder.Entity<EmployeeCompetence>().HasKey(x => new { x.EmployeeId, x.CompetenceId });
-        modelBuilder.Entity<ShiftAssignment>().HasKey(x => new { x.ShiftId, x.EmployeeId });
-        modelBuilder.Entity<ShiftRequirement>().HasKey(x => new { x.ShiftId, x.CompetenceId });
-        modelBuilder.Entity<Competence>().HasIndex(x => x.Name).IsUnique();
-        modelBuilder.Entity<Absence>().HasIndex(x => new { x.EmployeeId, x.From, x.To });
-        modelBuilder.Entity<AuditEvent>().HasIndex(x => x.OccurredAtUtc);
-        modelBuilder.Entity<UserAccount>().HasIndex(x => x.Username).IsUnique();
-        modelBuilder.Entity<UserAccount>().Property(x => x.Username).HasMaxLength(100);
-        modelBuilder.Entity<UserAccount>().Property(x => x.Role).HasMaxLength(40);
+        modelBuilder.Entity<Absence>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Id).HasColumnType("int");
+            b.Property(x => x.EmployeeId).HasColumnType("int");
+            b.Property(x => x.From).HasColumnType("date");
+            b.Property(x => x.To).HasColumnType("date");
+            b.Property(x => x.Type).HasColumnType("int");
+            b.Property(x => x.Note).HasColumnType("nvarchar(max)");
+            b.Property(x => x.Approved).HasColumnType("bit");
+            b.HasIndex(x => new { x.EmployeeId, x.From, x.To });
+            b.HasOne(x => x.Employee).WithMany(x => x.Absences).HasForeignKey(x => x.EmployeeId).OnDelete(DeleteBehavior.Cascade).IsRequired();
+        });
+
+        modelBuilder.Entity<AuditEvent>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Id).HasColumnType("bigint");
+            b.Property(x => x.OccurredAtUtc).HasColumnType("datetime2");
+            b.Property(x => x.Action).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.EntityType).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.EntityId).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.Actor).HasColumnType("nvarchar(max)");
+            b.Property(x => x.Reason).HasColumnType("nvarchar(max)");
+            b.Property(x => x.DetailsJson).HasColumnType("nvarchar(max)");
+            b.HasIndex(x => x.OccurredAtUtc);
+        });
+
+        modelBuilder.Entity<Competence>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Id).HasColumnType("int");
+            b.Property(x => x.Name).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.Category).IsRequired().HasColumnType("nvarchar(max)");
+            b.HasIndex(x => x.Name).IsUnique();
+        });
+
+        modelBuilder.Entity<Employee>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Id).HasColumnType("int");
+            b.Property(x => x.Name).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.Role).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.Department).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.Authorization).HasColumnType("nvarchar(max)");
+            b.Property(x => x.PositionPercent).HasPrecision(5, 2).HasColumnType("decimal(5,2)");
+            b.Property(x => x.MaxWeeklyHours).HasPrecision(5, 2).HasColumnType("decimal(5,2)");
+            b.Property(x => x.IsActive).HasColumnType("bit");
+        });
+
+        modelBuilder.Entity<EmployeeCompetence>(b =>
+        {
+            b.HasKey(x => new { x.EmployeeId, x.CompetenceId });
+            b.Property(x => x.EmployeeId).HasColumnType("int");
+            b.Property(x => x.CompetenceId).HasColumnType("int");
+            b.Property(x => x.Level).HasColumnType("int");
+            b.Property(x => x.ValidUntil).HasColumnType("date");
+            b.HasOne(x => x.Competence).WithMany(x => x.Employees).HasForeignKey(x => x.CompetenceId).OnDelete(DeleteBehavior.Cascade).IsRequired();
+            b.HasOne(x => x.Employee).WithMany(x => x.Competences).HasForeignKey(x => x.EmployeeId).OnDelete(DeleteBehavior.Cascade).IsRequired();
+        });
+
+        modelBuilder.Entity<Shift>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Id).HasColumnType("int");
+            b.Property(x => x.Date).HasColumnType("date");
+            b.Property(x => x.ShiftType).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.Department).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.StartTime).HasColumnType("time");
+            b.Property(x => x.Hours).HasPrecision(4, 2).HasColumnType("decimal(4,2)");
+            b.Property(x => x.MinimumStaff).HasColumnType("int");
+            b.Property(x => x.IsPublished).HasColumnType("bit");
+            b.Property(x => x.IsCritical).HasColumnType("bit");
+        });
+
+        modelBuilder.Entity<ShiftAssignment>(b =>
+        {
+            b.HasKey(x => new { x.ShiftId, x.EmployeeId });
+            b.Property(x => x.ShiftId).HasColumnType("int");
+            b.Property(x => x.EmployeeId).HasColumnType("int");
+            b.HasOne(x => x.Employee).WithMany(x => x.ShiftAssignments).HasForeignKey(x => x.EmployeeId).OnDelete(DeleteBehavior.Cascade).IsRequired();
+            b.HasOne(x => x.Shift).WithMany(x => x.Assignments).HasForeignKey(x => x.ShiftId).OnDelete(DeleteBehavior.Cascade).IsRequired();
+        });
+
+        modelBuilder.Entity<ShiftRequirement>(b =>
+        {
+            b.HasKey(x => new { x.ShiftId, x.CompetenceId });
+            b.Property(x => x.ShiftId).HasColumnType("int");
+            b.Property(x => x.CompetenceId).HasColumnType("int");
+            b.Property(x => x.MinimumCount).HasColumnType("int");
+            b.Property(x => x.MinimumLevel).HasColumnType("int");
+            b.Property(x => x.RequiredRole).HasColumnType("nvarchar(max)");
+            b.Property(x => x.IsCritical).HasColumnType("bit");
+            b.HasOne(x => x.Competence).WithMany(x => x.ShiftRequirements).HasForeignKey(x => x.CompetenceId).OnDelete(DeleteBehavior.Cascade).IsRequired();
+            b.HasOne(x => x.Shift).WithMany(x => x.Requirements).HasForeignKey(x => x.ShiftId).OnDelete(DeleteBehavior.Cascade).IsRequired();
+        });
+
+        modelBuilder.Entity<UserAccount>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Id).HasColumnType("int");
+            b.Property(x => x.Username).IsRequired().HasMaxLength(100).HasColumnType("nvarchar(100)");
+            b.Property(x => x.PasswordHash).IsRequired().HasColumnType("nvarchar(max)");
+            b.Property(x => x.Role).IsRequired().HasMaxLength(40).HasColumnType("nvarchar(40)");
+            b.Property(x => x.IsActive).HasColumnType("bit");
+            b.Property(x => x.FailedLoginAttempts).HasColumnType("int");
+            b.Property(x => x.LockedUntilUtc).HasColumnType("datetime2");
+            b.Property(x => x.CreatedAtUtc).HasColumnType("datetime2");
+            b.Property(x => x.LastLoginAtUtc).HasColumnType("datetime2");
+            b.Property(x => x.EmployeeId).HasColumnType("nvarchar(max)");
+            b.HasIndex(x => x.Username).IsUnique();
+        });
     }
 }
